3bbb0012f1ee8ddf17a88df244f4725e5679578e
debo <debojyoti.it@gmail.com>
2019-07-08 22:18:56 -0500

    Changed Data Profiling, Data Quality Check and Generic DQC Scripts

--- Utilities/GenericDQCAndProfiling/dataProfiling/checkDataProfiling.py
+++ Utilities/GenericDQCAndProfiling/dataProfiling/checkDataProfiling.py
@@ -12,38 +12,63 @@
 
 class DataProfiling:
 
-	def __init__(self,*args):
+    def __init__(self,*args):
         pass
 
-	def findMax(self,spark,df,columnName,format):
-        df.createOrReplaceTempView("findMax")
-        maxValueDf = spark.sql("select max({columnName}) from findMax".format(columnName=columnName))
-        maxValue = maxValueDf.collect()[0]
-		return maxValue
 
-	def findMin(self,spark,df,columnName,format):
-        df.createOrReplaceTempView("findMin")
-		minValueDf = spark.sql("select min({columnName}) from findMin".format(columnName=columnName))
-		minValue = minValueDf.collect()[0]
-		return minValue
 
-	def findMaxLength(self,spark,df,columnName,format):
+    def findMaxNum(self,spark,df,columnName):
+        df.createOrReplaceTempView("findMaxNum")
+        maxValueNumDf = spark.sql("select max({columnName}) from findMaxNum".format(columnName=columnName))
+        maxValueNum = maxValueNumDf.collect()[0]
+        return maxValueNum
+
+
+
+    def findMinNum(self,spark,df,columnName):
+        df.createOrReplaceTempView("findMinNum")
+        minValueNumDf = spark.sql("select min({columnName}) from findMinNum".format(columnName=columnName))
+        minValueNum = minValueNumDf.collect()[0]
+        return minValueNum
+
+    def findMaxDate(self,spark,df,columnName,format):
+        df.createOrReplaceTempView("findMaxDate")
+        maxValueDateDf = spark.sql("select max(unix_timestamp(trim({columnName}),'{format}')) from findMaxDate".format(columnName=columnName,format=format))
+        maxValueDate = maxValueDateDf.collect()[0]
+        return maxValueDate
+
+
+
+    def findMinDate(self,spark,df,columnName,format):
+        df.createOrReplaceTempView("findMinDate")
+        minValueDateDf = spark.sql("select min(unix_timestamp(trim({columnName}),'{format}')) from findMinDate".format(columnName=columnName,format=format))
+        minValueDate = minValueDateDf.collect()[0]
+        return minValueDate
+
+
+
+    def findMaxLength(self,spark,df,columnName):
         df.createOrReplaceTempView("findMaxLength")
-		findMaxLengthDf = spark.sql("select min({columnName}) from findMaxLength".format(columnName=columnName))
-		maxLength = findMaxLengthDf.collect()[0]
-		return maxLength
+        findMaxLengthDf = spark.sql("select min({columnName}) from findMaxLength".format(columnName=columnName))
+        maxLength = findMaxLengthDf.collect()[0]
+        return maxLength
 
-	def findMinLength(self,spark,df,columnName,format):
+
+
+    def findMinLength(self,spark,df,columnName):
         df.createOrReplaceTempView("findMinLength")
-		findMinLengthDf = spark.sql("select min({columnName}) from findMinLength".format(columnName=columnName))
-		minLength = findMinLengthDf.collect()[0]
-		return minLength
+        findMinLengthDf = spark.sql("select min({columnName}) from findMinLength".format(columnName=columnName))
+        minLength = findMinLengthDf.collect()[0]
+        return minLength
 
-	def findNumberOfNulls(self,spark,df,columnName,format):
+
+
+    def findNumberOfNulls(self,spark,df,columnName):
         df.createOrReplaceTempView("findNumberOfNulls")
-		findNumberOfNullsDf = spark.sql("select count(*) from findNumberOfNulls where {columnName} is NULL".format(columnName=columnName))
-		numberOfNulls = findNumberOfNullsDf.collect()[0]
-		return numberOfNulls
+        findNumberOfNullsDf = spark.sql("select count(*) from findNumberOfNulls where {columnName} is NULL".format(columnName=columnName))
+        numberOfNulls = findNumberOfNullsDf.collect()[0]
+        return numberOfNulls
+
 
 
 if(__name__ == "__main__"):
--- Utilities/GenericDQCAndProfiling/generic_dqc_profile.py
+++ Utilities/GenericDQCAndProfiling/generic_dqc_profile.py
@@ -5,12 +5,13 @@
 
   MODIFICATION LOG
   =========================================================================
-  Date     Who                Ver.  Proj             Reason
+  Date       Who              Ver.  Proj         Reason
   =========================================================================
-  20190319 Debo Mukherjee      v1   Hackathon       Initial draft
+  20190319 Debo Mukherjee      v1   Hackathon        Initial draft
 '''
 
 import json
+import logging
 from datetime import datetime
 import os
 import sys
@@ -18,105 +19,114 @@ import getopt
 from pprint import pprint
 from pyspark.sql import SparkSession
 from dataQuality.checkDataQuality import DataValidation
+from dataProfiling.checkDataProfiling import DataProfiling
 
 
+##Setting Up Logging Parameters
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def getCommandLineArgs():
-    	global srcFileName
-    	global profileFileName
 
-    	srcFileName = None
-    	profileFileName = None
+    logging.info("Starting -- Validation of Command Line Arguments")
 
-    	try:
-    		opts, args = getopt.getopt(sys.argv[1:],"s:p:")
-    	except Exception as e:
-    		sys.exit("ERROR: Failed to parse argument - {e}".format(e=e))
+    global srcFileName
+    global profileFileName
+    srcFileName = None
+    profileFileName = None
 
-    	for opt,arg in opts:
-    		if (opt == "-s"):
-    			srcFileName = arg
-    		elif (opt == "-p"):
-    			profileFileName = arg
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"s:p:")
+    except Exception as e:
+        logging.error("Invalid Arguments - {e}".format(e=e))
+        sys.exit()
 
-    	if (srcFileName is None or srcFileName ==''):
-    		sys.exit("Source File name not provided with argument -s")
-    	elif (profileFileName is None or profileFileName ==''):
-    		sys.exit("Profile File name not provided with argument -p")
+    for opt,arg in opts:
+        if (opt == "-s"):
+            srcFileName = arg
+        elif (opt == "-p"):
+            profileFileName = arg
 
-    	return True
+    if (srcFileName is None or srcFileName ==''):
+        logging.error("Source File name not provided with argument -s")
+        sys.exit()
+    elif (profileFileName is None or profileFileName ==''):
+        logging.error("Profile File name not provided with argument -p")
+        sys.exit()
 
+    logging.info("Finished -- Validation of Command Line Arguments")
+    print("*****************************************************************************************************")
+
+    return True
 
 
 
 def checkFileLocation():
-	if(not(os.path.isfile(srcFileName))):
-		sys.exit("ERROR: Source File '{srcFileName}' not found".format(srcFileName=srcFileName))
-	elif(not(os.path.isfile(profileFileName))):
-		sys.exit("ERROR: Profile File '{profileFileName}' not found".format(profileFileName=profileFileName))
-	else:
-		print("Found the Source File '{srcFileName}' and profile File '{profileFileName}'".format(srcFileName=srcFileName,profileFileName=profileFileName))
+    if(not(os.path.isfile(srcFileName))):
+        logging.error("Source File '{srcFileName}' not found".format(srcFileName=srcFileName))
+        sys.exit()
+    elif(not(os.path.isfile(profileFileName))):
+        sys.exit("ERROR: Profile File '{profileFileName}' not found".format(profileFileName=profileFileName))
+    else:
+        print("Found the Source File '{srcFileName}' and profile File '{profileFileName}'".format(srcFileName=srcFileName,profileFileName=profileFileName))
 
-	return True
+    return True
 
 
 
 
 def parsePropertiesFile():
-	global data_properties
-	data_properties	= []
-	global fileDelimiter
-	try:
-		with open(profileFileName) as property_file:
-			data_item = json.load(property_file)
-	except Exception as e:
-		sys.exit("ERROR: Failed to read JSON property file - {e}".format(e=e))
+    global data_properties
+    data_properties    = []
+    global fileDelimiter
+    try:
+        with open(profileFileName) as property_file:
+            data_item = json.load(property_file)
+    except Exception as e:
+        sys.exit("ERROR: Failed to read JSON property file - {e}".format(e=e))
 
-	for profile_data in data_item['profile']:
-		data_properties.append(profile_data)
+    for profile_data in data_item['profile']:
+        data_properties.append(profile_data)
 
-	fileDelimiter = data_item['delimiter']
-	return True
-
+    fileDelimiter = data_item['delimiter']
+    return True
 
 
 
 def validatingFieldProperties():
-	defaultDateFormat = "yyyy-MM-dd"
-	defaultTimeStampFormat = "yyyy-MM-dd HH:mm:ss"
-	possibleDataTypes = ['text','date','timestamp','categorical','numeric']
+    defaultDateFormat = "yyyy-MM-dd"
+    defaultTimeStampFormat = "yyyy-MM-dd HH:mm:ss"
+    possibleDataTypes = ['text','date','timestamp','categorical','numeric']
 
-	for property in data_properties:
-		if(not set(['field','type']) <= set(property.keys())):
-			sys.exit("ERROR: Failed as the the property is missing keys 'field' or 'type' - {property}".format(property =  property.keys()))
+    for property in data_properties:
+        if(not set(['field','type']) <= set(property.keys())):
+            sys.exit("ERROR: Failed as the the property is missing keys 'field' or 'type' - {property}".format(property =  property.keys()))
 
-		elif(not property['type'] in possibleDataTypes):
-			sys.exit("ERROR: Failed as the data type mentioned '{dataType}' does not belong to {possibleDataTypes}".format(dataType = property['type'], possibleDataTypes = possibleDataTypes))
+        elif(not property['type'] in possibleDataTypes):
+            sys.exit("ERROR: Failed as the data type mentioned '{dataType}' does not belong to {possibleDataTypes}".format(dataType = property['type'], possibleDataTypes = possibleDataTypes))
 
-		elif(property['type'] == 'date' and not set(['format']) <= set(property.keys())):
-			sys.exit("ERROR: Failed as the field type is set to 'date' but the format is missing - {property}".format(property =  property.keys()))
+        elif(property['type'] == 'date' and not set(['format']) <= set(property.keys())):
+            sys.exit("ERROR: Failed as the field type is set to 'date' but the format is missing - {property}".format(property =  property.keys()))
 
-		elif(property['type'] == 'timestamp' and not set(['format']) <= set(property.keys())):
-			sys.exit("ERROR: Failed as the field type is set to 'timestamp' but the format is missing - {property}".format(property =  property.keys()))
+        elif(property['type'] == 'timestamp' and not set(['format']) <= set(property.keys())):
+            sys.exit("ERROR: Failed as the field type is set to 'timestamp' but the format is missing - {property}".format(property =  property.keys()))
 
-	return True
+    return True
 
 
 
 
 def invokeSparkSession():
-	global spark
-	spark = SparkSession.builder.appName("{srcFileName} Data Quality Check and Profiling at {dateTime}".format(srcFileName = srcFileName,dateTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S'))).getOrCreate()
-	return True
+    global spark
+    spark = SparkSession.builder.appName("{srcFileName} Data Quality Check and Profiling at {dateTime}".format(srcFileName = srcFileName,dateTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S'))).getOrCreate()
+    return True
 
 
 
 
 def readFile():
-	global readFileDf
-	readFileDf = spark.read.option("delimiter",fileDelimiter).option("header",True).option("inferSchema",False).csv(srcFileName)
-
+    global readFileDf
+    readFileDf = spark.read.option("delimiter",fileDelimiter).option("header",True).option("inferSchema",False).csv(srcFileName)
 
 
 
@@ -141,26 +151,37 @@ def dataQualityCheck():
                 elif(dqcType == 'duplicate'):
                     dataElement['dqcDuplicateFlag'] = dqc.validateDuplicate(spark, readFileDf , dataElement['field'])
 
-                dqDpResult.append(dataElement)
-        else:
-            dqDpResult.append(dataElement)
-        print dqDpResult
+        dqDpResult.append(dataElement)
+    print dqDpResult
 
-		
-		
-		
+
+
 def dataProfiling():
+    dpr = DataProfiling()
     for dataElement in data_properties:
-	    if(dataElement['type'] == 'date'):
-		    
-    
+        if(dataElement['type'] == 'date'):
+            dataElement['dpfDateMinValue'] = dpr.findMinDate(spark,readFileDf,dataElement['field'],dataElement['format'])
+            dataElement['dpfDateMaxValue'] = dpr.findMaxDate(spark,readFileDf,dataElement['field'],dataElement['format'])
+            dataElement['dpfDateNullCount'] = dpr.findNumberOfNulls(spark,readFileDf,dataElement['field'])
+
+        elif(dataElement['type'] == 'numeric'):
+            dataElement['dpfNumMinValue'] = dpr.findMinNum(spark,readFileDf,dataElement['field'])
+            dataElement['dpfNumMaxValue'] = dpr.findMaxNum(spark,readFileDf,dataElement['field'])
+            dataElement['dpfNumNullCount'] = dpr.findNumberOfNulls(spark,readFileDf,dataElement['field'])
+
+        elif(dataElement['type'] == 'text'):
+            dataElement['dpfTxtMinLen'] = dpr.findMinLength(spark,readFileDf,dataElement['field'])
+            dataElement['dpfTxtMaxLen'] = dpr.findMaxLength(spark,readFileDf,dataElement['field'])
+            dataElement['dpfTxtNullCount'] = dpr.findNumberOfNulls(spark,readFileDf,dataElement['field'])
+
+        dqDpResult.append(dataElement)
+    print dqDpResult
 
 
 
 def writeOutputDQC():
     with open('data.json', 'w') as outfile:
-        json.dump(dqDpResult, outfile)
-
+         json.dump(dqDpResult, outfile)
 
 
 
@@ -171,75 +192,74 @@ The main script starts below
 '''
 def main():
 
-    	print(2*"\n")
-    	print("Starting Job to Profile File based on metadata")
-    	print("*****************************************************************************************************")
+    print(2*"\n")
+    logging.info("Starting Job to Profile File based on metadata")
+    print("*****************************************************************************************************")
 
 
     #Step1 - Check and Get Command line arguments
-    	print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Starting -- Validation of Command Line Arguments")
-    	getCommandLineArgs()
-    	print("The name of the Source file is   : " + srcFileName)
-    	print("The name of the Profile file is  : " + profileFileName)
-    	print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Finished -- Validation of Command Line Arguments")
-    	print("*****************************************************************************************************")
+    getCommandLineArgs()
+    logging.info("The name of the Source file is    : " + srcFileName)
+    logging.info("The name of the Profile file is    : " + profileFileName)
+
 
 
     #Step2 - Check source file and profile file location
-    	print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Starting -- Validation of Source and Profile File Paths")
-    	checkFileLocation()
-    	print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Finished -- Validation of Source and Profile File Paths")
-    	print("*****************************************************************************************************")
+    logging.info("Starting -- Validation of Source and Profile File Paths")
+    checkFileLocation()
+    logging.info("Finished -- Validation of Source and Profile File Paths")
+    print("*****************************************************************************************************")
 
 
     #Step3 - Parsing the profile file
-    	print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Starting -- Parsing of Profile File")
-    	parsePropertiesFile()
-    	print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Finished -- Parsing of Profile File")
-    	print("*****************************************************************************************************")
+    logging.info("Starting -- Parsing of Profile File")
+    parsePropertiesFile()
+    logging.info("Finished -- Parsing of Profile File")
+    print("*****************************************************************************************************")
 
 
     #Step4 - Validating the properties file
-    	print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Starting -- Validation of Field Properties")
-    	validatingFieldProperties()
-    	print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Finished -- Validation of Field Properties")
-    	print("*****************************************************************************************************")
+    logging.info("Starting -- Validation of Field Properties")
+    validatingFieldProperties()
+    logging.info("Finished -- Validation of Field Properties")
+    print("*****************************************************************************************************")
 
 
     #Step5 - Invoke Spark Session
-        print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Starting -- Invoking Spark Session")
-        invokeSparkSession()
-        print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Finished -- Invoking Spark Session")
-        print("*****************************************************************************************************")
+    logging.info("Starting -- Invoking Spark Session")
+    invokeSparkSession()
+    logging.info("Finished -- Invoking Spark Session")
+    print("*****************************************************************************************************")
 
 
     #Step6 - Reading the Source File in a Spark DataFrame
-    	print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Starting -- Reading Source File")
-    	readFile()
-    	print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Finished -- Reading Source File")
-    	print("*****************************************************************************************************")
+    logging.info("Starting -- Reading Source File")
+    readFile()
+    logging.info("Finished -- Reading Source File")
+    print("*****************************************************************************************************")
 
 
     #Step7 - Performing Data Quality Check
-        print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Starting -- Data Quality Check")
-        dataQualityCheck()
-        print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Finished -- Data Quality Check")
-        print("*****************************************************************************************************")
+    logging.info("Starting -- Data Quality Check")
+    dataQualityCheck()
+    logging.info("Finished -- Data Quality Check")
+    print("*****************************************************************************************************")
 
 
-    #Step8 - Performing Data Quality Check
-        print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Starting -- Write to JSON")
-        writeOutputDQC()
-        print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Finished -- Write to JSON")
-        print("*****************************************************************************************************")
-		
-		
-    #Step9 - Performing Data Profiling
-        print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Starting -- Data Profiling")
-        dataProfiling()
-        print("["+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+"]"+"Finished -- Data Profiling")
-        print("*****************************************************************************************************")
+    #Step8 - Performing Data Profiling
+    logging.info("Starting -- Data Profiling")
+    dataProfiling()
+    logging.info("Finished -- Data Profiling")
+    print("*****************************************************************************************************")
+
+
+    #Step9 - Performing Data Quality Check
+    logging.info("Starting -- Write to JSON")
+    writeOutputDQC()
+    logging.info("Finished -- Write to JSON")
+    print("*****************************************************************************************************")
+
 
 
 if(__name__ == "__main__"):
-	main()
+    main()
