0b84098e078c6498cc9a683709d4f87517fff7fe
debojyotimukherjee <debojyoti.it@gmail.com>
2019-11-25 22:51:47 -0600

    Changes for SCD

---
+++ Utilities/Scd/scd/detectChange.py
@@ -0,0 +1,21 @@
+'''
+  Purpose: Class and Methods for Detecting Changes for Slowly Changing Dimension
+
+  MODIFICATION LOG
+  =========================================================================
+  Date     Who                Ver.  Proj             Reason
+  =========================================================================
+  20190715 Debo Mukherjee      v1   Hackathon       Initial draft
+'''
+
+###Class defined for data validation
+
+class DetectChange:
+    def __init__(self,*args):
+        pass
+    
+    def detectChange(spark, sourceColumns, targetColumns, scdAuditColumns):
+        
+
+if(__name__ == "__main__"):
+    pass
